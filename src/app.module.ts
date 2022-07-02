c5b1377dcd6bb49549857c07a96f981c576399f9
Term <40206647+TermTaepaisitphongse@users.noreply.github.com>
2022-07-02 14:56:24 +0700

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,15 @@ import { MedicalRecordModule } from './medicalRecords/medicalRecord.module';
 import { ToDoModule } from './toDos/toDo.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     MedicalRecordModule,
     ToDoModule,
-    MongooseModule.forRoot(''),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     AuthModule,
   ],
   controllers: [AppController],
